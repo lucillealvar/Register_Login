55f48b09afb16e1f5dac0f72eafb5fe930c22b0a
Lucille Alvar <lucille.alvar@gmail.com>
2025-01-17 12:50:49 -0500

    On branch develop Your branch is up to date with 'origin/develop'.
    Changes to be committed:
    	modified:   front-end/src/App.jsx
    	new file:   front-end/src/context/userContext.jsx
    	modified:   front-end/src/pages/Login.jsx
    	modified:   front-end/src/pages/Login.scss

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,19 +1,21 @@
 import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import UserProvider from "./context/userContext";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
-
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="register" element={<Signup />}></Route>
-        <Route path="login" element={<Login />}></Route>
-      </Routes>
-    </BrowserRouter>
+    <UserProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="register" element={<Signup />}></Route>
+          <Route path="login" element={<Login />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </UserProvider>
   );
 }
 
--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -1,8 +1,41 @@
-import React from "react";
+import React, { useState, useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 import "./Login.scss";
 
+import { UserContext } from "../context/userContext.jsx";
+
 function Login() {
+  const [userData, setUserData] = useState({
+    email: "",
+    password: "",
+  });
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const { setCurrentUser } = useContext(UserContext);
+
+  const inputHandler = (e) => {
+    setUserData((prevState) => {
+      return { ...prevState, [e.target.name]: e.target.value };
+    });
+  };
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    setError("");
+    try {
+      const response = await axios.post(
+        `${import.meta.env.VITE_REACT_APP_BASE_URL}/users/login`,
+        userData
+      );
+      const user = await response.data;
+      setCurrentUser(user);
+      navigate("/");
+    } catch (err) {
+      setError(err.response.data.message);
+    }
+  };
   return (
     <div className="login">
       <div className="login_title">
@@ -10,21 +43,28 @@ function Login() {
         <h3>Enter your credentials to Login</h3>
       </div>
       <div className="login_form">
-        <form className="login_form-field">
+        <form className="login_form-field" onSubmit={handleLogin}>
+          {error && <p className="login_form-field-error">{error}</p>}
           <input
             type="text"
             className="email"
             placeholder="Email"
             autoComplete="off"
             name="email"
+            value={userData.email}
+            onChange={inputHandler}
           />
           <input
             type="password"
             className="password"
             placeholder="Password"
             name="password"
+            value={userData.password}
+            onChange={inputHandler}
           />
-          <button type="submit" className="btn">Login</button>
+          <button type="submit" className="btn">
+            Login
+          </button>
         </form>
         <div className="login_form-signup">
           <p>Don't have an account?</p>
